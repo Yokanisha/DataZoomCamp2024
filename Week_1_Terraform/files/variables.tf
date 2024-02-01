317f132f55c3fdfccb2f073c11ae3f4c0aff0b8e
Fatih <95353768+Yokanisha@users.noreply.github.com>
2024-02-01 15:35:02 +0100

    Add files via upload

---
+++ Week_1_Terraform/files/variables.tf
@@ -0,0 +1,34 @@
+variable "credentials" {
+    description = "my Credentials"
+    default = "C:/Users/Armut/Desktop/ZoomCamp-GCP/keys/my-creds.json"
+}
+
+variable "project" {
+    description = "Project"
+    default = "evident-beacon-412117"
+}
+
+variable "region" {
+    description = "Region"
+    default = "europe-west1"
+}
+
+variable "location" {
+    description = "Project Location"
+    default = "EU"
+}
+
+variable "bq_dataset_name" {
+    description = "My BigQuery Dataset Name"
+    default = "demo_dataset"
+}
+
+variable "gcs_bucket_name" {
+    description = "My Storage Bucket Name"
+    default = "evident-beacon-412117-terra-bucket"
+}
+
+variable "gcs_storage_class" {
+    description = "Bucket Storage Class"
+    default = "STANDARD"
+}
